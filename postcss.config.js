1c6d0651445df8ad74a1bf022ffe9f5194273feb
Shefqet Lika <qeti.lika@gmail.com>
2021-12-01 15:49:32 +0100

    update ruby version and webpacker

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,4 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== "production";
-
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-    ],
-  },
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
