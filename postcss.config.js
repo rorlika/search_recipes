357747a92feb2f987b5286746f03a38f7c23a535
Shefqet Lika <qeti.lika@gmail.com>
2021-12-01 15:36:48 +0100

    update webpack config

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,20 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
+
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
